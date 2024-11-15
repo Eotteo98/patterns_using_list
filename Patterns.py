1c6a6525ba73e24dfa8795254e3cbfd9fb240787
Eotteo98 <cosma.teo@yahoo.com>
2024-11-15 18:48:38 +0100

    Add files via upload
    making patterns with lists

---
+++ Patterns.py
@@ -0,0 +1,39 @@
+# for i in range (65, 70):
+#   for j in range(65,i+1):
+#     print(chr(j),end= " ")
+#   print()
+
+
+
+
+# for i in range (0, 10):
+#    for j in range(0,i+1):
+#      print("* ",end= " ")
+#    print("\r")
+
+# print("hello Teo",end=" ,What day is it?")
+
+n = 5
+
+
+# for i in range(n):
+    
+#     for j in range(n):
+#         print("* ", end=" ")
+#     print("\r")  
+
+
+# school_hours = [1,2,3,4,5,6]
+
+# import random
+
+# choice = random.choice(school_hours)
+# print(choice)
+
+# Subjects = ["English", "Maths", "Physics", "Economy", "Chemistry", "Dutch", "History", "German" ]
+# Subject = random.choice(Subjects)
+# print(Subject)
+
+# print("you have been invited to diner", Subject )
+
+if true print("login")
